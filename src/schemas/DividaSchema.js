2351512f51b190ab3e5dac8048e23747af6ceccb
Gustalex <alexandregustavo00@gmail.com>
2024-04-16 22:43:57 -0300

    Revert "Merge remote-tracking branch 'origin/main' into frontEnd"
    This reverts commit fd387c32451374fdbcd4de9e09359ef75b32fed0, reversing
    changes made to 7c1f878c56b3e146e2cd85bd08cf067c782f5d91.

--- src/schemas/DividaSchema.js
+++ src/schemas/DividaSchema.js
@@ -2,12 +2,11 @@ import mongoose from "mongoose";
 
 const dividaSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    id_devedor: { type: mongoose.Schema.Types.ObjectId, ref: "pessoas", required: true },
-    id_fiador: { type: mongoose.Schema.Types.ObjectId, ref: "pessoas", required: true },
+    devedor: { type: mongoose.Schema.Types.ObjectId, ref: "pessoas", required: true },
+    fiador: { type: mongoose.Schema.Types.ObjectId, ref: "pessoas", required: true },
     valor: { type: Number, required: true },
     status: { type: Boolean, default: false },
     dataCriacao: { type: Date, default: Date.now },
-    prazo: { type: Date},
 }, { versionKey: false });
 
 const dividaModel = mongoose.model("dividas", dividaSchema);
